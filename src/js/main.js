082eea6bdbb489a9112c4ca470201fe4d5153998
MohammedAl-rowad <rowadwo@gmail.com>
2020-10-02 13:40:19 +0300

    import works :)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
-const func = async () => {
-  alert('rowad')
+import { compose } from 'underscore'
+const func = () => {
+  alert('rowadz')
+}
+const func2 = () => {
+  alert('rowad 02')
 }
 
-func()
+const allFuncs = compose(func, func2)
+
+allFuncs()

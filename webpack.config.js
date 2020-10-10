73236b6458f1ea120f3a190c1b73b22358cd1bde
MohammedAl-rowad <rowadwo@gmail.com>
2020-10-10 16:04:21 +0300

    fix test

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.m?js$/,
+        test: /\.*js$/,
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
